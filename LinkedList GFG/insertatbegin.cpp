08c08a7ec7d37dde0e27060355d3d679f2994f91
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-02-13 11:21:20 +0530

    Insert at BEGIN LL

--- LinkedList GFG/insertatbegin.cpp
+++ LinkedList GFG/insertatbegin.cpp
@@ -2,10 +2,25 @@
 using namespace std;
 
 /*
-
+ 10 20 30
 */
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int x): data(x),next(NULL){}
+};
+
+Node* insertBegin(Node *head,int x){
+    Node* temp = new Node (x);
+    temp ->next = head;
+    return temp;
+}
+
 
 int main()
 {
+
+    return 0;
 
 }
