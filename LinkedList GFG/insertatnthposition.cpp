e87f3ef948a05f31169f6b0da249f86b6ebdb243
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-03-02 00:59:48 +0530

    InsertionatNth Pos Done

--- LinkedList GFG/insertatnthposition.cpp
+++ LinkedList GFG/insertatnthposition.cpp
@@ -1,11 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-Node insertPos(Node *head , int pos, int data){
-     
+// Define the Node class
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Constructor
+    Node(int val) : data(val), next(nullptr) {}
+};
+
+// Function to insert a node at a specific position
+Node* insertPos(Node* head, int pos, int data) {
+    Node* temp = new Node(data);
+
+    if (pos == 1) {
+        temp->next = head;
+        return temp;
+    }
+
+    Node* curr = head;
+    for (int i = 1; i < pos - 1 && curr != nullptr; i++) {
+        curr = curr->next;
+    }
+
+    if (curr == nullptr)
+        return head;
+
+    temp->next = curr->next;
+    curr->next = temp;
+
+    return head;
 }
 
-int main(){
-    cout<<"Hello World";
+int main() {
+    // Create a linked list node
+    Node* head = new Node(1);
+
+    // Insert nodes at different positions
+    head = insertPos(head, 1, 2);
+    head = insertPos(head, 2, 3);
+    head = insertPos(head, 2, 4);
+    head = insertPos(head,)
+
+    // Display the linked list
+    Node* curr = head;
+    while (curr != nullptr) {
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+
     return 0;
 }
