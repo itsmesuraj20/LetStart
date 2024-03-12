93adee4441cfe3f732def455279b933345dce06f
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-03-12 23:48:35 +0530

    SortedInsert.cpp

---
+++ LinkedList GFG/SortedInsert.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+    int data;
+    Node *next;
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+Node * sortInsert(Node * head , int target){
+    Node * temp = new Node(target);
+    if(head ==NULL);
+    {
+        head = temp;
+        temp->next = NULL;
+
+        return temp;
+    }
+
+    Node * curr = head;
+    while(curr!= NULL){
+        if(curr->data <target){
+            
+        }
+    }
+
+}
+
+int main(){
+    Node * head = new Node(10);
+    head ->next = new Node(20);
+    head ->next->next = new Node(30);
+    head ->next ->next->next = new Node(40);
+
+    int x;
+    cin>>x;
+
+
+    head = sortInsert(head,x);
+
+
+    while(head != NULL){
+        cout<<head ->data <<" ";
+        head = head ->next;
+    }
+
+    return 0;
+}
