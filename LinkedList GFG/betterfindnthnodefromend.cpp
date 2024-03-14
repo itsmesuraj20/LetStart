83c570808ac2a1e6290450b1c277831f38c433d5
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-03-14 22:53:10 +0530

    CF Soln

--- LinkedList GFG/betterfindnthnodefromend.cpp
+++ LinkedList GFG/betterfindnthnodefromend.cpp
@@ -1,8 +1,20 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-
+ //Codeforces Solution 
 int main(){
-    
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y,z;
+        cin>>x>>y>>z;
+        
+        if(x + y == z){
+            cout<<"+"<<endl;
+        }
+        else{
+            cout<<"-"<<endl;
+        }
+    }
     return 0;
 }
